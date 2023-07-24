e95d3d3224c9adc1854c3d1e3bd6a6e03576a6fe
Prabhu G <133245461+imprabhu002@users.noreply.github.com>
2023-07-24 18:08:05 +0530

    Create 50. Pow(x, n).py

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,23 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1.0
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        half_pow = self.myPow(x, n // 2)
+
+        if n % 2 == 0:
+            return half_pow * half_pow
+        else:
+            return half_pow * half_pow * x
+
+# Test the implementation
+if __name__ == "__main__":
+    solution = Solution()
+    x = 2.0
+    n = 10
+    result = solution.myPow(x, n)
+    print(f"{x} raised to the power {n} is: {result}")
