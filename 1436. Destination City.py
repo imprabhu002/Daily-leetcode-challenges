3d459b20528f696c8484965e063fd447e311eeba
Prabhu G <133245461+imprabhu002@users.noreply.github.com>
2023-12-15 19:25:15 +0530

    Create 1436. Destination City.py

---
+++ 1436. Destination City.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start = set()
+        destination = set()
+        for city in paths:
+            start.add(city[0])
+            destination.add(city[1])
+        return destination.difference(start).pop()
